97326d509c01dad65c6c9161b67d1c7c90b76ca6
Brandon Lara <larabrandon408@gmail.com>
2021-10-19 15:33:26 -0700

    Updates

---
+++ precache-manifest.b1411e4857060185863a6d7b2fec95a7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7be7a4cfa8d323255ca4479ad6a7bd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49a324bbd941d19f8caa",
+    "url": "/static/css/2.046f5d7a.chunk.css"
+  },
+  {
+    "revision": "98d283b59039535ae64f",
+    "url": "/static/css/main.9963cac0.chunk.css"
+  },
+  {
+    "revision": "49a324bbd941d19f8caa",
+    "url": "/static/js/2.48ac5fb7.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.48ac5fb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98d283b59039535ae64f",
+    "url": "/static/js/main.7ef1e7a1.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  },
+  {
+    "revision": "c547106fd5fa89faa148cc4b8bdc176f",
+    "url": "/static/media/backround.c547106f.mp4"
+  },
+  {
+    "revision": "fae6bcc5209cb5e6433fbd5d67603980",
+    "url": "/static/media/matrix.fae6bcc5.jpeg"
+  },
+  {
+    "revision": "703817eba3c5f78f771ac5437982874f",
+    "url": "/static/media/python.703817eb.jpeg"
+  },
+  {
+    "revision": "2bcf8bd8b561beac61225a5ae88c3a4a",
+    "url": "/static/media/testimgTwo.2bcf8bd8.jpeg"
+  }
+]);
